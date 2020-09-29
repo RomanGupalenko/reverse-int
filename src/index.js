64e6eae057bdb20c9c6480141d60e303c0920cdc
romangupalenko <gupalenko.98@mail.ru>
2020-09-29 18:39:50 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let nToString = String(n);
+    if (nToString[0] === "-") nToString = nToString.slice(1,);
+    let result = nToString.split("").reverse().join("");
+    return result;
 }
